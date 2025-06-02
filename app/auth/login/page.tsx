9bda715f34bdbce50a5492b2c9422964a44c3747
Luis Angel Alba Alfaro <pirules_neta@yahoo.com.mx>
2025-06-01 22:17:12 -0600

    change on login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,11 @@
-import { LoginForm } from '@/components/auth/login-form';
+import { LoginForm } from "@/components/auth/login-form";
+import { currentUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
-    return <LoginForm />;
-};
-
-export default LoginPage;
+export default async function LoginPage() {
+  const user = await currentUser();
+  if (user) {
+    redirect("/page"); // o '/' según tu flujo
+  }
+  return <LoginForm />;
+}
