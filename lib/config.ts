b391322c49b9f4f8d414abb916688b00ac7151b7
Pirules <pirules_neta@yahoo.com.mx>
2025-06-12 10:54:57 -0600

    🚀 Deploy automático con configuración personalizada

---
+++ lib/config.ts
@@ -0,0 +1,24 @@
+export const pageConfig = {
+  "navbar": 1,
+  "hero": 2,
+  "content": 2,
+  "footer": 2,
+  "name": "Lola Test",
+  "colors": {
+    "primary": {
+      "light": "#60A5FA",
+      "DEFAULT": "#2563EB",
+      "dark": "#1D4ED8"
+    },
+    "secondary": {
+      "light": "#A78BFA",
+      "DEFAULT": "#7C3AED",
+      "dark": "#6D28D9"
+    },
+    "accent": {
+      "light": "#34D399",
+      "DEFAULT": "#059669",
+      "dark": "#047857"
+    }
+  }
+};
